b9e307dc04344adde15221c43a5ece9f953e7a4b
Shiyu Zhang <xanatower@users.noreply.github.com>
2016-11-13 20:40:23 +1100

    Update examples.ino

--- examples.ino
+++ examples.ino
@@ -30,14 +30,10 @@ void loop() {
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
-
-
-
 /*
   DigitalReadSerial
  Reads a digital input on pin 2, prints the result to the serial monitor
  */
-
 int pushButton = 2;
 
 void setup() {
@@ -49,19 +45,14 @@ void loop() {
   Serial.println(buttonState);
   delay(1);        // delay in between reads for stability
 }
-
-
 /*
  Fade
-
- This example shows how to fade an LED on pin 9
+This example shows how to fade an LED on pin 9
  using the analogWrite() function.
-
+ 
  The analogWrite() function uses PWM,
  a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
  */
-
 int led = 9;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
@@ -75,10 +66,8 @@ void setup() {
 void loop() {
   // set the brightness of pin 9:
   analogWrite(led, brightness);
-
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
-
   // reverse the direction of the fading at the ends of the fade:
   if (brightness == 0 || brightness == 255) {
     fadeAmount = -fadeAmount ;
@@ -86,13 +75,11 @@ void loop() {
   // wait for 30 milliseconds to see the dimming effect
   delay(30);
 }
-
 /*
   ReadAnalogVoltage
   Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
 */
 
-// the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
 }
@@ -106,8 +93,6 @@ void loop() {
   // print out the value you read:
   Serial.println(voltage);
 }
-
-
 /* Blink without Delay
 
  Turns on and off a light emitting diode (LED) connected to a digital
@@ -116,14 +101,11 @@ void loop() {
  */
 
 const int ledPin =  13;      // the number of the LED pin
-
 // Variables will change :
 int ledState = LOW;             // ledState used to set the LED
-
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
 unsigned long previousMillis = 0;        // will store last time LED was updated
-
 // constants won't change :
 const long interval = 1000;           // interval at which to blink (milliseconds)
 
@@ -134,31 +116,24 @@ void setup() {
 
 void loop() {
   // here is where you'd put code that needs to be running all the time.
-
   // check to see if it's time to blink the LED; that is, if the
   // difference between the current time and last time you blinked
   // the LED is bigger than the interval at which you want to
   // blink the LED.
   unsigned long currentMillis = millis();
-
   if (currentMillis - previousMillis >= interval) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
-
     // set the LED with the ledState of the variable:
     digitalWrite(ledPin, ledState);
   }
 }
-
-
-
 /*
   State change detection (edge detection)
 
@@ -186,7 +161,6 @@ void setup() {
 void loop() {
   // read the pushbutton input pin:
   buttonState = digitalRead(buttonPin);
-
   // compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
     // if the state has changed, increment the counter
@@ -221,15 +195,12 @@ void loop() {
   }
 
 }
-
-
 /*
   Analog input, analog output, serial output
 
  Reads an analog input pin, maps the result to a range from 0 to 255
  and uses the result to set the pulsewidth modulation (PWM) of an output pin.
  Also prints the results to the serial monitor.
-
  */
 
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
@@ -250,28 +221,19 @@ void loop() {
   outputValue = map(sensorValue, 0, 1023, 0, 255);
   // change the analog out value:
   analogWrite(analogOutPin, outputValue);
-
   // print the results to the serial monitor:
   Serial.print("sensor = ");
   Serial.print(sensorValue);
   Serial.print("\t output = ");
   Serial.println(outputValue);
-
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
   delay(2);
 }
-
-
-
 /*
-
   Smoothing
-AVERAGE
-
-
-*/
+AVERAGE*/
 
 const int numReadings = 10;
 
@@ -279,7 +241,6 @@ int readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
 int total = 0;                  // the running total
 int average = 0;                // the average
-
 int inputPin = A0;
 
 void setup() {
@@ -300,30 +261,25 @@ void loop() {
   total = total + readings[readIndex];
   // advance to the next position in the array:
   readIndex = readIndex + 1;
-
   // if we're at the end of the array...
   if (readIndex >= numReadings) {
     // ...wrap around to the beginning:
     readIndex = 0;
   }
-
   // calculate the average:
   average = total / numReadings;
   // send it to the computer as ASCII digits
   Serial.println(average);
   delay(1);        // delay in between reads for stability
 }
-
 /*
   Switch statement
 */
-
 void loop() {
   // read the sensor:
   int sensorReading = analogRead(A0);
   // map the sensor range to a range of four options:
   int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
-
   // do something different depending on the
   // range value:
   switch (range) {
@@ -342,5 +298,3 @@ void loop() {
   }
   delay(1);        // delay in between reads for stability
 }
-
-
